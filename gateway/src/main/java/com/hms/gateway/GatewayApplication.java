688555ac4e7093b5a4fb4d088ed6b8e3b7ccfcab
RavenTheshadow <hogiathang2@gmail.com>
2025-04-09 14:23:46 +0700

    Fix bug không tìm thấy server Eureka

--- gateway/src/main/java/com/hms/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/hms/gateway/GatewayApplication.java
@@ -1,7 +1,15 @@
 package com.hms.gateway;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+
+import java.time.Duration;
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class GatewayApplication {
@@ -10,4 +18,16 @@ public class GatewayApplication {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder) {
+		return routeLocatorBuilder.routes()
+				.route("patient-service", r -> r.path("/patient/**")
+						.filters(f -> f.rewritePath("/patient/(?<segment>.*)", "/${segment}")
+								.filter((exchange, chain) -> {
+									System.out.println("Request Time: " + LocalDateTime.now());
+									return chain.filter(exchange);
+								})
+						)
+						.uri("lb://PATIENT-SERVICE")).build();
+	}
 }
